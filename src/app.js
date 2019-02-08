414159b80b1f3e3eef331c24e8b9ac0d58099864
davejmacm <davejmacm@gmail.com>
2019-02-08 12:22:05 +0000

    basic set up sorted, DOM loaded listener

---
+++ src/app.js
@@ -0,0 +1,6 @@
+const Film = require('./models/film.js');
+
+
+document.addEventListener('DOMContentLoaded', ()=>{
+  console.log("DOM loaded - let's go");
+})
---
+++ src/models/film.js
@@ -0,0 +1,10 @@
+const Request = require('../helpers/request.js');
+const PubSub = require('../helpers/pub_sub.js');
+
+const Film = function(){
+  this.data = null;
+};
+
+
+
+module.exports = Film;
